0962b91949cff688d5ca6af50547be5d9e693a53
Jerrinald <jerrinald95190@live.fr>
2024-02-18 15:38:49 +0100

    card method

--- card.py
+++ card.py
@@ -7,8 +7,14 @@ from datetime import datetime, timedelta
 from typing import List, Dict
 
 class Card(BaseModel):
-    id: Optional[int] = None  
-    category: Category
+    id: Optional[int] = Field(default=None, example=0)  # Set default value to None
+    category: Category = Category.FIRST
     question: str
     answer: str
     tag: Optional[str] = None
+
+    def set_category(self):
+        currentCategoryIndex = Category.__members__[self.category].value
+        nextCategoryIndex = (currentCategoryIndex + 1) % len(Category)
+        nextCategory = list(Category)[nextCategoryIndex]
+        self.category = nextCategory
